c1107f1689d2d864116261cb2e43c068c36f4003
essential-blog-3393 <jcpleitez@outlook.com>
2020-09-22 14:19:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,7 +11,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  ["I", "am", "really", "learning"] #unshift("wow")
+  ["I", "am", "really", "learning"] Array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
